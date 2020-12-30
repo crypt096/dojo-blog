be0702a81a145924169f35e7ecfb3f7d24979a70
crypt0 <acvjetan@gmail.com>
2020-12-30 22:45:29 +0100

    Template refs.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="home">home</div>
-  <p>My name is {{ name }} and my age is : {{ age }}</p>
+  <p ref="p">My name is {{ name }} and my age is : {{ age }}</p>
   <button @click="handleClick">Click me</button>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
+    console.log(this);
     let name = "Alex";
     let age = 24;
 
+    const p = ref(null);
+
     const handleClick = () => {
-      console.log("You clicked me");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello,ninjas";
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };

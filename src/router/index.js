7b45217089f2d916ba5383e650bb6c585830e4f2
crypt0 <acvjetan@gmail.com>
2020-12-31 20:26:48 +0100

    Reusable composable functions.

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import {
   createWebHistory
 } from 'vue-router'
 import Home from '../views/Home.vue'
+import Details from '../views/Details.vue'
 
 const routes = [{
   path: '/',
   name: 'Home',
   component: Home
-}, ]
+}, {
+  path: '/posts/:id',
+  name: 'Details',
+  component: Details,
+  props: true
+}]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    home
     <h1>Home</h1>
     <div v-if="error">
       {{ error }}
@@ -14,29 +13,15 @@
 
 <script>
 import PostList from "../components/PostList";
-import { ref } from "vue";
+import getPosts from "../composables/getPosts";
+
 export default {
   name: "Home",
   components: {
     PostList,
   },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("No data available");
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
-
+    const { posts, error, load } = getPosts();
     load();
     return { posts, error };
   },
